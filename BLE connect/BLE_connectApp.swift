f0e2ce7c6c34b0c10f24a3cf75af4ba753ceeb34
IParamonikhin <paramonikhin.ivan@yandex.ru>
2024-04-14 16:28:59 +0300

    Initial Commit

---
+++ BLE connect/BLE_connectApp.swift
@@ -0,0 +1,17 @@
+//
+//  BLE_connectApp.swift
+//  BLE connect
+//
+//  Created by Иван on 14.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct BLE_connectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
